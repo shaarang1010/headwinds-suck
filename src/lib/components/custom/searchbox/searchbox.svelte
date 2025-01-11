ab3821bcc3a2b695a0cbd0688e9821e684a64335
Shaarang T <shaarang.1910@gmail.com>
2025-01-11 23:26:16 +1100

    add geocoding type

---
+++ src/lib/components/custom/searchbox/searchbox.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import * as Command from "$lib/components/ui/command";
+    import { type GeoCodingAPIResponse} from "../../../../types/geo-coding"
+    let { data }:{
+        data: GeoCodingAPIResponse[] ; // define json api data.
+    } = $props();
+</script>
+
+<Command.Command>
+
+</Command.Command>
