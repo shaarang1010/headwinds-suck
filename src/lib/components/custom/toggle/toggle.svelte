794b4f4e33c68b7527f483614ab90d89086281f1
Shaarang T <shaarang.1910@gmail.com>
2025-01-15 23:20:57 +1100

    rename toggle component to avoid base component confusion

---
+++ src/lib/components/custom/toggle/toggle.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import * as ToggleGroup from '$lib/components/ui/toggle-group';
+	let {
+		options,
+		value
+	}: {
+		value: string;
+		options: { label: string; val: string }[];
+	} = $props();
+</script>
+
+<ToggleGroup.Root
+	type="single"
+	{value}
+	onValueChange={(val) => {
+		if (val) {
+			value = val;
+		}
+	}}
+>
+	<div class="flex w-[100%] flex-row space-x-4">
+		{#each options as option}
+			<ToggleGroup.Item value={option.val}>{option.label}</ToggleGroup.Item>
+		{/each}
+	</div>
+</ToggleGroup.Root>
